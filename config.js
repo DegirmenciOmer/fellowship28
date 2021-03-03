870feae171820c1223c83e1853702079188f4d61
Omer Degirmenci <rumidegirmenci0@gmail.com>
2021-03-03 20:42:53 +0100

    add gitignore

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  MONGODB:
+    'mongodb+srv://OmerD:Kurabiye1.@cluster0.xovff.mongodb.net/MERNG-Fellowship28?authSource=admin&replicaSet=atlas-suvovb-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true',
+  SECRET_KEY: 'some very secret key',
+};
+
+// module.exports = {
+//   MONGODB:
+//     'mongodb+srv://classsed:6lxxxlexaTslHLPe@cluster0-pcsru.mongodb.net/merng?retryWrites=true',
+//   SECRET_KEY: 'some very secret key',
+// };
